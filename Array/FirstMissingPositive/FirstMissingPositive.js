f81b6772d907166cf9a8f5cfec028d8cf17b37e5
yangbo <yangbo@mistong.com>
2021-06-17 14:43:00 +0800

    docs: add array FirstMissingPositive Algo

---
+++ Array/FirstMissingPositive/FirstMissingPositive.js
@@ -0,0 +1,19 @@
+const firstMissingPositive = (nums) => {
+  const n = nums.length;
+  let i = 0;
+
+  while(i < n) {
+    if (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+      [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
+    } else {
+      i++;
+    }
+  }
+
+  for(let i = 0; i < n; i++) {
+    if (nums[i] != i + 1) {
+      return i + 1;
+    }
+  }
+  return nums.length + 1;
+}
