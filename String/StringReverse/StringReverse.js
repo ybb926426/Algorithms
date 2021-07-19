ff86c80266e41a2bb447658df5642cdab8a9bf3b
yangbo <yangbo@mistong.com>
2021-07-19 09:41:57 +0800

    feat: add mysql docs

---
+++ String/StringReverse/StringReverse.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+  let res = 0;
+
+  while(x != 0) {
+    //每次取末尾数字
+    let tmp = x % 10;
+    //判断是否 大于 最大32位整数
+    if (res>214748364 || (res==214748364 && tmp>7)) {
+      return 0;
+    }
+    //判断是否 小于 最小32位整数
+    if (res<-214748364 || (res==-214748364 && tmp<-8)) {
+      return 0;
+    }
+    res = res * 10 + tmp;
+    x = Math.floor(x / 10);
+  }
+
+  return res;
+};
